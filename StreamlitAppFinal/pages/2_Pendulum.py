fc45236f2fce7ccd9ca36875191f81e69f91077a
julia-dunn <jmdunn0905@gmail.com>
2025-05-02 16:05:39 -0400

    update to allow for varying number of setups

--- StreamlitAppFinal/pages/2_Pendulum.py
+++ StreamlitAppFinal/pages/2_Pendulum.py
@@ -5,8 +5,9 @@ st.write("Here is where you can imput your data from the pendulum experiment")
 st.markdown(" ### Changing Angle")
 st.write("Here, input the length of your pendulum, the different angles of elevation you used, and the time it took to complete 5 oscillations.")
 st.number_input("Length of pendulum [m]", value=None, placeholder="(meters)")
+n = st.number_input("Number of Angles:", value=1)
 c1, c2, c3, c4 = st.columns(4)
-for i in range(6):
+for i in range(n):
     c1.number_input(f"Angle of Elevation {i+1}", value=None, placeholder="(degrees)")
     c2.number_input(f"Angle {i+1} - trial 1", value=None, placeholder="Time (s)")
     c3.number_input(f"Angle {i+1} - trial 2", value=None, placeholder="Time (s)")
@@ -14,8 +15,9 @@ for i in range(6):
 st.markdown(" ### Changing Length")
 st.write("Here, input the angle of elevation, the different lengths of the pendulum, and the time it took to complete 5 oscillations.")
 st.number_input("Angle of Elevation (degrees)", value=None, placeholder="(degrees)")
+m = st.number_input("Number of Lengths:", value=1)
 c1, c2, c3, c4 = st.columns(4)
-for i in range(6):
+for i in range(m):
     c1.number_input(f"Length {i+1}", value=None, placeholder="(meters)")
     c2.number_input(f"Length {i+1} - trial 1", value=None, placeholder="Time (s)")
     c3.number_input(f"Length {i+1} - trial 2", value=None, placeholder="Time (s)")
